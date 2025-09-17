e4ff8619b86a4d144e0d658acc4c85d165c669a4
IT24101361 <it24101361@my.sliit.lk>
2025-09-17 12:55:54 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24101361_Lab06/IT24101361_Lab_06.R
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\USER\\Desktop\\IT24101361_Lab06")
+#Q1: Learning Platform
+n <- 50
+p <- 0.85
+prob_Q1 <- 1 - pbinom(46, size= n, prob= p)
+prob_Q1
+#Q2: call Center
+lambda <- 12
+prob_Q2 <- dpois(15, lambda)
+prob_Q2
